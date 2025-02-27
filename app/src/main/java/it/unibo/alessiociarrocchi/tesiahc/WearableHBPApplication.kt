c18dd0651feb87dbadb449583a93580abfb3ec4b
ff225 <frafranco16@gmail.com>
2025-02-27 17:17:26 +0100

    minor fix

--- app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/WearableHBPApplication.kt
+++ app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/WearableHBPApplication.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import it.unibo.alessiociarrocchi.tesiahc.data.AppContainerImpl
-import it.unibo.alessiociarrocchi.tesiahc.data.MyHealthConnectManager
 import it.unibo.alessiociarrocchi.tesiahc.receivers.HealthDataReceiver
 import it.unibo.alessiociarrocchi.tesiahc.receivers.HourNotificationReceiver
 import it.unibo.alessiociarrocchi.tesiahc.receivers.LocationReceiver
@@ -13,23 +12,17 @@ import it.unibo.alessiociarrocchi.tesiahc.receivers.LocationReceiver
 class WearableHBPApplication : Application() {
 
     lateinit var appContainer: AppContainerImpl
-    val healthConnectManager by lazy {
-        MyHealthConnectManager(this)
-    }
 
     override fun onCreate() {
         super.onCreate()
 
-        // TODO move all notification here.
+
         val channel = NotificationChannel(
             HealthDataReceiver.CHANNEL_ID,
             HealthDataReceiver.CHANNEL_NAME,
             NotificationManager.IMPORTANCE_LOW
         )
-        val notificationManager = getSystemService(
-            Context.NOTIFICATION_SERVICE
-        ) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+
 
         // consiglio di effettuare la misurazione
         val channel1 = NotificationChannel(
@@ -37,10 +30,7 @@ class WearableHBPApplication : Application() {
             HourNotificationReceiver.CHANNEL_NAME,
             NotificationManager.IMPORTANCE_HIGH
         )
-        val notificationManager1 = getSystemService(
-            Context.NOTIFICATION_SERVICE
-        ) as NotificationManager
-        notificationManager1.createNotificationChannel(channel1)
+
 
         // localizzazione GPS
         val channel2 = NotificationChannel(
@@ -48,10 +38,13 @@ class WearableHBPApplication : Application() {
             LocationReceiver.CHANNEL_NAME,
             NotificationManager.IMPORTANCE_LOW
         )
-        val notificationManager2 = getSystemService(
+
+        val notificationManager = getSystemService(
             Context.NOTIFICATION_SERVICE
         ) as NotificationManager
-        notificationManager2.createNotificationChannel(channel2)
+        notificationManager.createNotificationChannels(listOf(channel, channel1, channel2))
+
+
         appContainer = AppContainerImpl(this)
     }
 }
