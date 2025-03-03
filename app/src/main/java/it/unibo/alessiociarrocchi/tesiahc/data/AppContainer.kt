ad651001af50ed4f482b1ca0999b2cb360b06d37
ff225 <frafranco16@gmail.com>
2025-03-03 12:17:39 +0100

    refactor: rename my* to *Repository

--- app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/data/AppContainer.kt
+++ app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/data/AppContainer.kt
@@ -3,7 +3,7 @@ package it.unibo.alessiociarrocchi.tesiahc.data
 import android.content.Context
 import it.unibo.alessiociarrocchi.tesiahc.data.repository.HeartRateRepository
 import it.unibo.alessiociarrocchi.tesiahc.data.repository.LocationRepository
-import it.unibo.alessiociarrocchi.tesiahc.data.repository.MyBloodPressureRepository
+import it.unibo.alessiociarrocchi.tesiahc.data.repository.BloodPressureRepository
 import it.unibo.alessiociarrocchi.tesiahc.data.repository.SettingsRepository
 
 
@@ -13,24 +13,24 @@ import it.unibo.alessiociarrocchi.tesiahc.data.repository.SettingsRepository
 
 interface AppContainer {
 
-    val mySettingsRepository: SettingsRepository
-    val myHeartRateRepository: HeartRateRepository
-    val myLocationRepository: LocationRepository
-    val myBloodPressureRepository: MyBloodPressureRepository
+    val settingsRepository: SettingsRepository
+    val heartRateRepository: HeartRateRepository
+    val locationRepository: LocationRepository
+    val bloodPressureRepository: BloodPressureRepository
 
 }
 
 class AppContainerImpl(context: Context) : AppContainer {
-    override val mySettingsRepository: SettingsRepository by lazy {
+    override val settingsRepository: SettingsRepository by lazy {
         SettingsRepository(LocalDatabase.getDatabase(context).settingsDao())
     }
-    override val myHeartRateRepository: HeartRateRepository by lazy {
+    override val heartRateRepository: HeartRateRepository by lazy {
         HeartRateRepository(LocalDatabase.getDatabase(context).heartRateAggregateDao())
     }
-    override val myLocationRepository: LocationRepository by lazy {
+    override val locationRepository: LocationRepository by lazy {
         LocationRepository(LocalDatabase.getDatabase(context).locationDao())
     }
-    override val myBloodPressureRepository: MyBloodPressureRepository by lazy {
-        MyBloodPressureRepository(LocalDatabase.getDatabase(context).bloodPressureDao())
+    override val bloodPressureRepository: BloodPressureRepository by lazy {
+        BloodPressureRepository(LocalDatabase.getDatabase(context).bloodPressureDao())
     }
 }
