fa43f029cc9e03d1031e45b0a10fa69cf9671a64
ff225 <frafranco16@gmail.com>
2025-03-04 18:02:17 +0100

    refactor: update Util functions for improved readability and consistency

--- app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/Util.kt
+++ app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/Util.kt
@@ -17,72 +17,72 @@ import java.util.Locale
 import android.graphics.Color as AndroidColor
 
 fun showInfoSnackbar(
-  scaffoldState: ScaffoldState,
-  scope: CoroutineScope,
-  message: String,
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope,
+    message: String,
 ) {
-  scope.launch {
-    scaffoldState.snackbarHostState.showSnackbar(
-      message = message,
-      duration = SnackbarDuration.Short
-    )
-  }
+    scope.launch {
+        scaffoldState.snackbarHostState.showSnackbar(
+            message = message,
+            duration = SnackbarDuration.Short
+        )
+    }
 }
 
-fun convertToLocalDateViaMilisecond(dateToConvert: Date, tz: Int): LocalDateTime {
-  return Instant.ofEpochMilli(dateToConvert.time)
-    .atZone(ZoneOffset.ofTotalSeconds(tz))
-    .toLocalDateTime()
+fun convertToLocalDateViaMillisecond(time: Long, tz: Int): LocalDateTime {
+    return Instant.ofEpochMilli(time)
+        .atZone(ZoneOffset.ofTotalSeconds(tz))
+        .toLocalDateTime()
 }
 
-fun Instant.toDate(): Date{
-  val myDate = Date.from(this)
-  return myDate
+fun Instant.toDate(): Date {
+    val myDate = Date.from(this)
+    return myDate
 }
 
 fun String?.toLong(): Long {
-  val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-  val date = formatter.parse(this)
-  return date!!.time
+    val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+    val date = formatter.parse(this)
+    return date!!.time
 }
 
 fun String?.toDate(): Date {
-  val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-  return formatter.parse(this)
+    val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    return formatter.parse(this)
 }
 
 fun Long?.toDate(): String {
-  val dateFormat = SimpleDateFormat("d MMM, y", Locale.getDefault())
-  return try {
-    dateFormat.format(this)
-  } catch (t: Throwable) {
-    t.printStackTrace()
-    ""
-  }
+    val dateFormat = SimpleDateFormat("d MMM, y", Locale.getDefault())
+    return try {
+        dateFormat.format(this)
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        ""
+    }
 }
 
 fun isOnline(context: Context): Boolean {
-  val connectivityManager =
-    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-  if (connectivityManager != null) {
-    val capabilities =
-      connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-    if (capabilities != null) {
-      if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (connectivityManager != null) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
 
-        return true
-      } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
 
-        return true
-      } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
 
-        return true
-      }
+                return true
+            }
+        }
     }
-  }
-  return false
+    return false
 }
 
 fun Color.Companion.fromHex(hex: String): Color {
-  return Color(AndroidColor.parseColor(hex))
+    return Color(AndroidColor.parseColor(hex))
 }
