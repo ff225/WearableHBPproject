a6c8512c973b375f5e92f8e42e181f889cf37054
ff225 <frafranco16@gmail.com>
2025-03-04 18:03:39 +0100

    feat: implement periodic work requests for data fetching and location updates in HomeScreen

--- app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/presentation/screen/HomeScreen.kt
+++ app/src/main/java/it/unibo/alessiociarrocchi/tesiahc/presentation/screen/HomeScreen.kt
@@ -11,11 +11,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import it.unibo.alessiociarrocchi.tesiahc.presentation.RouteDestination
 import it.unibo.alessiociarrocchi.tesiahc.worker.GetDataFromHC
+import it.unibo.alessiociarrocchi.tesiahc.worker.GetLatestLocation
+import it.unibo.alessiociarrocchi.tesiahc.worker.SendDataToFirebase
+import java.util.concurrent.TimeUnit
 
 object HomeScreen : RouteDestination {
     override val route: String = "home_screen"
@@ -24,10 +29,45 @@ object HomeScreen : RouteDestination {
 
 @Composable
 fun HomeScreen(navController: NavController? = null) {
-
     val context = LocalContext.current
+
+    val periodicGetDataFromHC: PeriodicWorkRequest =
+        PeriodicWorkRequest.Builder(GetDataFromHC::class.java, 30, TimeUnit.MINUTES)
+            .addTag("PeriodicGetDataFromHC")
+            .build()
+
+    val periodicGetCurrentLocationRequest: PeriodicWorkRequest =
+        PeriodicWorkRequest.Builder(GetLatestLocation::class.java, 15, TimeUnit.MINUTES)
+            .addTag("PeriodicGetCurrentLocation")
+            .build()
+
+    val periodicSendDataToFirebase: PeriodicWorkRequest =
+        PeriodicWorkRequest.Builder(SendDataToFirebase::class.java, 1, TimeUnit.HOURS)
+            .addTag("PeriodicSendDataToFirebase")
+            .build()
+
+    WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+        "getDataFromHC",
+        ExistingPeriodicWorkPolicy.KEEP,
+        periodicGetDataFromHC
+    )
+
+    WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+        "getCurrentLocation",
+        ExistingPeriodicWorkPolicy.KEEP,
+        periodicGetCurrentLocationRequest
+    )
+
+    WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+        "sendDataToFirebase",
+        ExistingPeriodicWorkPolicy.KEEP,
+        periodicSendDataToFirebase
+    )
+
+
     MyScaffold(
         title = HomeScreen.title,
+        navController = navController
     ) {
 
         Column(
@@ -44,8 +84,18 @@ fun HomeScreen(navController: NavController? = null) {
                         .addTag("getDataFromHC")
                         .build()
 
+                val getCurrentLocationRequest: WorkRequest =
+                    OneTimeWorkRequest.Builder(GetLatestLocation::class.java)
+                        .addTag("getCurrentLocation")
+                        .build()
+
+                val sendDataToFirebase: WorkRequest =
+                    OneTimeWorkRequest.Builder(SendDataToFirebase::class.java)
+                        .addTag("sendDataToFirebase")
+                        .build()
+
                 WorkManager.getInstance(context).enqueue(
-                    getDataFromHC
+                    listOf(getDataFromHC, getCurrentLocationRequest, sendDataToFirebase)
                 )
 
             }) {
